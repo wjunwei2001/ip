62481f0c5cbc0c28e3de286437bfe3fada399630
Junwei <wangjunwei38@gmail.com>
2026-01-23 00:29:48 +0800

    Mark as Done feature
    Add the ability to mark and unmark tasks as done. Created a new Task class

--- src/main/java/Pallo.java
+++ src/main/java/Pallo.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Pallo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         
         System.out.println("    ____________________________________________________________");
@@ -24,18 +24,63 @@ public class Pallo {
                 break;
             } else if (trimmedInput.equalsIgnoreCase("list")) {
                 System.out.println("    ____________________________________________________________");
+                System.out.println("     Here are the tasks in your list:");
                 if (taskCount == 0) {
                     System.out.println("     No tasks stored yet.");
                 } else {
                     for (int i = 0; i < taskCount; i++) {
-                        System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                        System.out.println("     " + (i + 1) + "." + tasks[i]);
                     }
                 }
                 System.out.println("    ____________________________________________________________");
                 System.out.println();
+            } else if (trimmedInput.toLowerCase().startsWith("mark ")) {
+                try {
+                    int taskNumber = Integer.parseInt(trimmedInput.substring(5).trim());
+                    if (taskNumber >= 1 && taskNumber <= taskCount) {
+                        tasks[taskNumber - 1].markAsDone();
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("     Nice! I've marked this task as done:");
+                        System.out.println("       " + tasks[taskNumber - 1]);
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println();
+                    } else {
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("     Invalid task number!");
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println();
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Please provide a valid task number!");
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println();
+                }
+            } else if (trimmedInput.toLowerCase().startsWith("unmark ")) {
+                try {
+                    int taskNumber = Integer.parseInt(trimmedInput.substring(7).trim());
+                    if (taskNumber >= 1 && taskNumber <= taskCount) {
+                        tasks[taskNumber - 1].markAsNotDone();
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("     OK, I've marked this task as not done yet:");
+                        System.out.println("       " + tasks[taskNumber - 1]);
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println();
+                    } else {
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println("     Invalid task number!");
+                        System.out.println("    ____________________________________________________________");
+                        System.out.println();
+                    }
+                } catch (NumberFormatException e) {
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Please provide a valid task number!");
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println();
+                }
             } else {
                 if (taskCount < 100) {
-                    tasks[taskCount] = trimmedInput;
+                    tasks[taskCount] = new Task(trimmedInput);
                     taskCount++;
                     System.out.println("    ____________________________________________________________");
                     System.out.println("     added: " + trimmedInput);
---
+++ src/main/java/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        String statusIcon = isDone ? "[X]" : "[ ]";
+        return statusIcon + " " + description;
+    }
+}
