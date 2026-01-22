cfb8da54aaf992c463e69961a36c8e169c9245b6
Junwei <wangjunwei38@gmail.com>
2026-01-23 00:22:40 +0800

    Ability to store and display text
    Add ability to store and display text back to the user when requested

--- src/main/java/Pallo.java
+++ src/main/java/Pallo.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Pallo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
         
         System.out.println("    ____________________________________________________________");
         System.out.println("     Hello! I'm Pallo");
@@ -13,19 +15,41 @@ public class Pallo {
         String input;
         while (true) {
             input = scanner.nextLine();
-            System.out.println("    ____________________________________________________________");
-            System.out.println("     " + input);
-            System.out.println("    ____________________________________________________________");
-            System.out.println();
+            String trimmedInput = input.trim();
             
-            if (input.trim().equalsIgnoreCase("bye")) {
+            if (trimmedInput.equalsIgnoreCase("bye")) {
+                System.out.println("    ____________________________________________________________");
+                System.out.println("     Bye. I will miss you!");
+                System.out.println("    ____________________________________________________________");
                 break;
+            } else if (trimmedInput.equalsIgnoreCase("list")) {
+                System.out.println("    ____________________________________________________________");
+                if (taskCount == 0) {
+                    System.out.println("     No tasks stored yet.");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("    ____________________________________________________________");
+                System.out.println();
+            } else {
+                if (taskCount < 100) {
+                    tasks[taskCount] = trimmedInput;
+                    taskCount++;
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     added: " + trimmedInput);
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println();
+                } else {
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println("     Sorry, I can only store up to 100 tasks!");
+                    System.out.println("    ____________________________________________________________");
+                    System.out.println();
+                }
             }
         }
         
-        System.out.println("    ____________________________________________________________");
-        System.out.println("     Bye. I will miss you!");
-        System.out.println("    ____________________________________________________________");
         scanner.close();
     }
 }
