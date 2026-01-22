444e21aa6e44f13ff4ff26e21fe48afd5984fa13
Junwei <wangjunwei38@gmail.com>
2026-01-23 00:13:37 +0800

    Echo feature
    Improve skeletal version so that it echos command entered by user, except for command bye

--- src/main/java/Pallo.java
+++ src/main/java/Pallo.java
@@ -1,7 +1,31 @@
+import java.util.Scanner;
+
 public class Pallo {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Pallo");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Hello! I'm Pallo");
+        System.out.println("     What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+        System.out.println();
+        
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            System.out.println("    ____________________________________________________________");
+            System.out.println("     " + input);
+            System.out.println("    ____________________________________________________________");
+            System.out.println();
+            
+            if (input.trim().equalsIgnoreCase("bye")) {
+                break;
+            }
+        }
+        
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     Bye. I will miss you!");
+        System.out.println("    ____________________________________________________________");
+        scanner.close();
     }
 }
