2e046082d50e0f83a3e0bdc6a8e997a654070d5e
Junwei <wangjunwei38@gmail.com>
2026-02-12 23:23:36 +0800

    Refactor command execution in Pallo class
    - Introduced a constant for the default file path to improve maintainability.
    - Modified executeCommand method to return responses instead of directly interacting with the UI.
    - Updated command handling methods to return messages for UI display.
    - Enhanced showMessage method in Ui class to handle multi-line messages for better output formatting.

--- src/main/java/pallo/Pallo.java
+++ src/main/java/pallo/Pallo.java
@@ -20,6 +20,7 @@ import pallo.ui.Ui;
  * manage, and track different types of tasks including todos, deadlines, and events.
  */
 public class Pallo {
+    private static final String DEFAULT_FILE_PATH = "data/pallo.txt";
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
@@ -28,7 +29,7 @@ public class Pallo {
      * Constructs a new Pallo application instance with the default file path.
      */
     public Pallo() {
-        this("data/pallo.txt");
+        this(DEFAULT_FILE_PATH);
     }
 
     /**
@@ -65,9 +66,10 @@ public class Pallo {
                     persistTasks();
                     ui.showGoodbye();
                     break;
-                } else {
-                    executeCommand(command);
                 }
+
+                String response = executeCommand(command);
+                ui.showMessage(response);
             } catch (PalloException e) {
                 ui.showError(e.getMessage());
             }
@@ -76,38 +78,40 @@ public class Pallo {
         ui.close();
     }
 
-    private void executeCommand(Command command) throws PalloException {
+    private String executeCommand(Command command) throws PalloException {
         boolean shouldSave = false;
+        String response;
+
         switch (command.getType()) {
         case LIST:
-            ui.showTaskList(tasks);
+            response = handleListCommand();
             break;
         case MARK:
-            handleMarkCommand(command);
+            response = handleMarkCommand(command);
             shouldSave = true;
             break;
         case UNMARK:
-            handleUnmarkCommand(command);
+            response = handleUnmarkCommand(command);
             shouldSave = true;
             break;
         case DELETE:
-            handleDeleteCommand(command);
+            response = handleDeleteCommand(command);
             shouldSave = true;
             break;
         case TODO:
-            handleTodoCommand(command);
+            response = handleTodoCommand(command);
             shouldSave = true;
             break;
         case DEADLINE:
-            handleDeadlineCommand(command);
+            response = handleDeadlineCommand(command);
             shouldSave = true;
             break;
         case EVENT:
-            handleEventCommand(command);
+            response = handleEventCommand(command);
             shouldSave = true;
             break;
         case FIND:
-            handleFindCommand(command);
+            response = handleFindCommand(command);
             break;
         default:
             throw new PalloException("OH NO!!! I'm sorry, but I don't know what that means :-(");
@@ -116,56 +120,79 @@ public class Pallo {
         if (shouldSave) {
             persistTasks();
         }
+
+        return response;
     }
 
-    private void handleMarkCommand(Command command) throws PalloException {
+    private String handleListCommand() throws PalloException {
+        StringBuilder response = new StringBuilder("Here are the tasks in your list:");
+        if (tasks.isEmpty()) {
+            response.append("\nNo tasks stored yet.");
+        } else {
+            for (int i = 0; i < tasks.size(); i++) {
+                response.append("\n" + (i + 1) + "." + tasks.getTask(i));
+            }
+        }
+        return response.toString();
+    }
+
+    private String handleMarkCommand(Command command) throws PalloException {
         int taskNumber = Parser.parseTaskNumber(command.getStringArgument());
         Task task = tasks.getTask(taskNumber - 1);
         task.markAsDone();
-        ui.showTaskMarked(task, true);
+        return "Nice! I've marked this task as done:\n  " + task;
     }
 
-    private void handleUnmarkCommand(Command command) throws PalloException {
+    private String handleUnmarkCommand(Command command) throws PalloException {
         int taskNumber = Parser.parseTaskNumber(command.getStringArgument());
         Task task = tasks.getTask(taskNumber - 1);
         task.markAsNotDone();
-        ui.showTaskMarked(task, false);
+        return "OK, I've marked this task as not done yet:\n  " + task;
     }
 
-    private void handleDeleteCommand(Command command) throws PalloException {
+    private String handleDeleteCommand(Command command) throws PalloException {
         int taskNumber = Parser.parseTaskNumber(command.getStringArgument());
         Task removedTask = tasks.removeTask(taskNumber - 1);
-        ui.showTaskRemoved(removedTask, tasks.size());
+        return "Noted. I've removed this task:\n  " + removedTask
+                + "\nNow you have " + tasks.size() + " tasks in the list.";
     }
 
-    private void handleTodoCommand(Command command) throws PalloException {
+    private String handleTodoCommand(Command command) throws PalloException {
         String description = command.getStringArgument();
-        if (description.isEmpty()) {
-            throw new PalloException("OH NO!!! The description of a todo cannot be empty.");
-        }
         Task newTask = new Todo(description);
         tasks.addTask(newTask);
-        ui.showTaskAdded(newTask, tasks.size());
+        return "Got it. I've added this task:\n  " + newTask
+                + "\nNow you have " + tasks.size() + " tasks in the list.";
     }
 
-    private void handleDeadlineCommand(Command command) throws PalloException {
+    private String handleDeadlineCommand(Command command) throws PalloException {
         String[] parts = command.getStringArrayArgument();
         Task newTask = new Deadline(parts[0], parts[1]);
         tasks.addTask(newTask);
-        ui.showTaskAdded(newTask, tasks.size());
+        return "Got it. I've added this task:\n  " + newTask
+                + "\nNow you have " + tasks.size() + " tasks in the list.";
     }
 
-    private void handleEventCommand(Command command) throws PalloException {
+    private String handleEventCommand(Command command) throws PalloException {
         String[] parts = command.getStringArrayArgument();
         Task newTask = new Event(parts[0], parts[1], parts[2]);
         tasks.addTask(newTask);
-        ui.showTaskAdded(newTask, tasks.size());
+        return "Got it. I've added this task:\n  " + newTask
+                + "\nNow you have " + tasks.size() + " tasks in the list.";
     }
 
-    private void handleFindCommand(Command command) {
+    private String handleFindCommand(Command command) {
         String keyword = command.getStringArgument();
         ArrayList<Task> matchingTasks = tasks.findTasks(keyword);
-        ui.showFoundTasks(matchingTasks);
+        StringBuilder response = new StringBuilder("Here are the matching tasks in your list:");
+        if (matchingTasks.isEmpty()) {
+            response.append("\nNo matching tasks found.");
+        } else {
+            for (int i = 0; i < matchingTasks.size(); i++) {
+                response.append("\n" + (i + 1) + "." + matchingTasks.get(i));
+            }
+        }
+        return response.toString();
     }
 
     private void persistTasks() {
@@ -185,108 +212,14 @@ public class Pallo {
     public String getResponse(String input) {
         try {
             Command command = Parser.parseCommand(input);
-            return executeCommandForGui(command);
+            if (command.getType() == CommandType.BYE) {
+                persistTasks();
+                return "Bye. I will miss you!";
+            }
+            return executeCommand(command);
         } catch (PalloException e) {
             return e.getMessage();
         }
-    }
-
-    private String executeCommandForGui(Command command) throws PalloException {
-        StringBuilder response = new StringBuilder();
-        boolean shouldSave = false;
-
-        switch (command.getType()) {
-        case BYE:
-            persistTasks();
-            response.append("Bye. I will miss you!");
-            break;
-        case LIST:
-            response.append("Here are the tasks in your list:\n");
-            if (tasks.isEmpty()) {
-                response.append("No tasks stored yet.");
-            } else {
-                for (int i = 0; i < tasks.size(); i++) {
-                    response.append((i + 1) + "." + tasks.getTask(i));
-                    if (i < tasks.size() - 1) {
-                        response.append("\n");
-                    }
-                }
-            }
-            break;
-        case MARK:
-            int markNum = Parser.parseTaskNumber(command.getStringArgument());
-            Task markTask = tasks.getTask(markNum - 1);
-            markTask.markAsDone();
-            response.append("Nice! I've marked this task as done:\n  " + markTask);
-            shouldSave = true;
-            break;
-        case UNMARK:
-            int unmarkNum = Parser.parseTaskNumber(command.getStringArgument());
-            Task unmarkTask = tasks.getTask(unmarkNum - 1);
-            unmarkTask.markAsNotDone();
-            response.append("OK, I've marked this task as not done yet:\n  " + unmarkTask);
-            shouldSave = true;
-            break;
-        case DELETE:
-            int deleteNum = Parser.parseTaskNumber(command.getStringArgument());
-            Task removedTask = tasks.removeTask(deleteNum - 1);
-            response.append("Noted. I've removed this task:\n  " + removedTask + "\n");
-            response.append("Now you have " + tasks.size() + " tasks in the list.");
-            shouldSave = true;
-            break;
-        case TODO:
-            String todoDesc = command.getStringArgument();
-            if (todoDesc.isEmpty()) {
-                throw new PalloException(
-                        "OH NO!!! The description of a todo cannot be empty.");
-            }
-            Task newTodo = new Todo(todoDesc);
-            tasks.addTask(newTodo);
-            response.append("Got it. I've added this task:\n  " + newTodo + "\n");
-            response.append("Now you have " + tasks.size() + " tasks in the list.");
-            shouldSave = true;
-            break;
-        case DEADLINE:
-            String[] dlParts = command.getStringArrayArgument();
-            Task newDeadline = new Deadline(dlParts[0], dlParts[1]);
-            tasks.addTask(newDeadline);
-            response.append("Got it. I've added this task:\n  " + newDeadline + "\n");
-            response.append("Now you have " + tasks.size() + " tasks in the list.");
-            shouldSave = true;
-            break;
-        case EVENT:
-            String[] evParts = command.getStringArrayArgument();
-            Task newEvent = new Event(evParts[0], evParts[1], evParts[2]);
-            tasks.addTask(newEvent);
-            response.append("Got it. I've added this task:\n  " + newEvent + "\n");
-            response.append("Now you have " + tasks.size() + " tasks in the list.");
-            shouldSave = true;
-            break;
-        case FIND:
-            String keyword = command.getStringArgument();
-            ArrayList<Task> matches = tasks.findTasks(keyword);
-            response.append("Here are the matching tasks in your list:\n");
-            if (matches.isEmpty()) {
-                response.append("No matching tasks found.");
-            } else {
-                for (int i = 0; i < matches.size(); i++) {
-                    response.append((i + 1) + "." + matches.get(i));
-                    if (i < matches.size() - 1) {
-                        response.append("\n");
-                    }
-                }
-            }
-            break;
-        default:
-            throw new PalloException(
-                    "OH NO!!! I'm sorry, but I don't know what that means :-(");
-        }
-
-        if (shouldSave) {
-            persistTasks();
-        }
-
-        return response.toString();
     }
 
     /**
@@ -295,6 +228,6 @@ public class Pallo {
      * @param args Command line arguments (not used).
      */
     public static void main(String[] args) {
-        new Pallo("data/pallo.txt").run();
+        new Pallo(DEFAULT_FILE_PATH).run();
     }
 }
